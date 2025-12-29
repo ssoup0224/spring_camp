6e68dd0d12eaa2465f9acaeb1b3887f8eeee6c52
정성국 <ssoup0224@gmail.com>
2025-12-30 00:15:32 +0900

    day 1

---
+++ demo/src/main/java/com/demo/controller/SampleController.java
@@ -0,0 +1,15 @@
+package com.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SampleController {
+
+    @ResponseBody
+    @RequestMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
