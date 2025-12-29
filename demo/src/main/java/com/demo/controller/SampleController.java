a092601f9235f894ac4ef4fef913f097820e7339
정성국 <ssoup0224@gmail.com>
2025-12-30 00:19:51 +0900

    assignment1

--- demo/src/main/java/com/demo/controller/SampleController.java
+++ demo/src/main/java/com/demo/controller/SampleController.java
@@ -12,4 +12,15 @@ public class SampleController {
     public String test() {
         return "test";
     }
+
+    @ResponseBody
+    @RequestMapping("/assignment")
+    public String assignment1() {
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.println(i + " * " + j + " = " + (i * j));
+            }
+        }
+        return "완료";
+    }
 }
