571257f51e90e39160f4ce28dc18b4b06725ec02
정성국 <ssoup0224@gmail.com>
2025-12-30 18:39:35 +0900

    finished day2 lesson

--- demo/src/main/java/com/demo/controller/DefaultRestController.java
+++ demo/src/main/java/com/demo/controller/DefaultRestController.java
@@ -1,7 +1,30 @@
 package com.demo.controller;
 
+import com.demo.ParamTest;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
+@RequestMapping("/api")
 public class DefaultRestController {
+    @RequestMapping("/test")
+    public Map<String, Object> test(@RequestParam Map<String, Object> params) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("test1", params.get("a"));
+        map.put("test2", 112233);
+        return map;
+    }
+
+    @RequestMapping("/test2")
+    public Map<String, Object> test2(ParamTest params) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("param1", params.getParam1());
+        map.put("param2", params.getParam2());
+        return map;
+    }
+
 }
