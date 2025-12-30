cba19f1a15367fa2956c0fa6df0f9883637a9cd1
정성국 <ssoup0224@gmail.com>
2025-12-30 18:12:35 +0900

    added mutiple controller

--- demo/src/main/java/com/demo/controller/DefaultController.java
+++ demo/src/main/java/com/demo/controller/DefaultController.java
@@ -1,12 +1,27 @@
 package com.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DefaultController {
     @RequestMapping("/index")
-    public String index() {
+    public String index(@RequestParam int a, @RequestParam int b, Model model) {
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+        int result = a + b;
+        model.addAttribute("result", result);
         return "index";
+    }
+
+    @RequestMapping("/multiple")
+    public String multiple(@RequestParam int a, @RequestParam int b, Model model) {
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+        int result = a * b;
+        model.addAttribute("result", result);
+        return "multiple";
     }
 }
