5666be1b2d044cb949870f69b4285970d51fff1d
정성국 <ssoup0224@gmail.com>
2026-01-21 22:59:47 +0900

    created board controllers and files

---
+++ demo/src/main/java/com/demo/controller/BoardRestController.java
@@ -0,0 +1,72 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/board")
+public class BoardRestController {
+
+    // 임시 정보 저장
+    List<Map<String, Object>> list = new ArrayList<>();
+    int id = 0;
+
+    @RequestMapping("/create")
+    public Map<String, Object> create(@RequestParam Map<String, Object> params) {
+        String title = (String) params.get("title");
+        String content = (String) params.get("content");
+        String author = (String) params.get("author");
+
+        // 아이디 값
+        params.put("id", ++id);
+        LocalDateTime now = LocalDateTime.now();
+        params.put("createdAt", now);
+
+        list.add(params);
+        System.out.println(list.toString());
+
+        Map<String, Object> map_result = new HashMap<>();
+        map_result.put("result_code", 200);
+
+        return map_result;
+    }
+
+    @RequestMapping("/list")
+    public Map<String, Object> list(@RequestParam Map<String, Object> params) {
+
+        Map<String, Object> map_result2 = new HashMap<>();
+        map_result2.put("result_code", 200);
+        map_result2.put("list", list);
+
+        return map_result2;
+    }
+
+    @RequestMapping("/detail/{id}")
+    public Map<String, Object> detail(@PathVariable String id) {
+
+        // String id = (String) params.get("id");
+
+        Map<String, Object> map_board = null;
+        for (Map<String, Object> each : list) {
+            String tempId = each.get("id") + "";
+            if (tempId.equals(id)) {
+                map_board = each;
+                break;
+            }
+        }
+
+        Map<String, Object> map_result2 = new HashMap<>();
+        map_result2.put("result_code", 200);
+        map_result2.put("list", map_board);
+
+        return map_result2;
+    }
+}
---
+++ demo/src/main/java/com/demo/controller/page/BoardPageController.java
@@ -0,0 +1,24 @@
+package com.demo.controller.page;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+public class BoardPageController {
+
+    @RequestMapping("/create")
+    public String create() {
+        return "board/create";
+    }
+
+    @RequestMapping("/detail")
+    public String detail() {
+        return "board/detail";
+    }
+
+    @RequestMapping("/list")
+    public String list() {
+        return "board/list";
+    }
+}
