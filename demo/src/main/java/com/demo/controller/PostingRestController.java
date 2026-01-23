4b0c6461ae476995aa7dc4bddb0e79471e5db816
정성국 <ssoup0224@gmail.com>
2026-01-23 19:12:27 +0900

    complete day 4

--- demo/src/main/java/com/demo/controller/PostingRestController.java
+++ demo/src/main/java/com/demo/controller/PostingRestController.java
@@ -1,9 +1,6 @@
 package com.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
@@ -11,11 +8,11 @@ import java.util.*;
 @RequestMapping("/api/posting")
 public class PostingRestController {
 
-    Map<Integer, Object> postings = new HashMap<>();
+    Map<Integer, Map<String, Object>> postings = new HashMap<>();
     int sequence = 1;
 
     @GetMapping("/create")
-    public Map<String, Object> create(@RequestParam Map<String, Object> map){
+    public Map<String, Object> create(@RequestParam Map<String, Object> map) {
         System.out.println("map : " + map);
         int id = sequence++;
         map.put("id", id);
@@ -34,22 +31,23 @@ public class PostingRestController {
     }
 
     @GetMapping("/list")
-    public Map<String, Object> list(){
+    public Map<String, Object> list() {
         List<Map<String, Object>> list = new ArrayList<>();
 
         // Method 1
-//        Set<Integer> keys = postings.keySet();
-//        Object[] array_key = keys.toArray();
-//        Arrays.sort(array_key);
-//
-//        for(Object key : array_key){
-//            list.add((Map<String, Object>) postings.get(key));
-//        }
+        // Set<Integer> keys = postings.keySet();
+        // Object[] array_key = keys.toArray();
+        // Arrays.sort(array_key);
+        //
+        // for(Object key : array_key){
+        // list.add((Map<String, Object>) postings.get(key));
+        // }
 
         // Method 2
-        for(int i=0; i<sequence; i++){
-           Object posting = postings.get(i);
-           if(posting != null) list.add((Map<String, Object>) posting);
+        for (int i = 0; i < sequence; i++) {
+            Map<String, Object> posting = postings.get(i);
+            if (posting != null)
+                list.add(posting);
         }
 
         Map<String, Object> map_result = new HashMap<>();
@@ -58,6 +56,39 @@ public class PostingRestController {
 
         System.out.println("map_result : " + map_result);
 
+        return map_result;
+    }
+
+    @GetMapping("/detail")
+    public Map<String, Object> detail(@RequestParam Integer id) {
+        Map<String, Object> map_result = new HashMap<>();
+        map_result.put("status", 200);
+        map_result.put("data", postings.get(id));
+        return map_result;
+    }
+
+    @PutMapping("")
+    public Map<String, Object> update(@RequestBody Map<String, Object> map) {
+        Integer id = Integer.parseInt(map.get("id") + "");
+        Map<String, Object> posting = postings.get(id);
+        if (map.get("title") != null)
+            posting.put("title", map.get("title"));
+        if (map.get("content") != null)
+            posting.put("content", map.get("content"));
+
+        postings.put(id, posting);
+
+        Map<String, Object> map_result = new HashMap<>();
+        map_result.put("status", 200);
+
+        return map_result;
+    }
+
+    @GetMapping("/delete")
+    public Map<String, Object> delete(@RequestParam Integer id) {
+        postings.remove(id);
+        Map<String, Object> map_result = new HashMap<>();
+        map_result.put("status", 200);
         return map_result;
     }
 }
