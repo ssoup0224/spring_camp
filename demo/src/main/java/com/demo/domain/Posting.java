4047bf352e9327d95c4aa65beb39736f176bbbd1
정성국 <ssoup0224@gmail.com>
2026-01-26 21:53:50 +0900

    changed posting eneity to extends auditingfields

--- demo/src/main/java/com/demo/domain/Posting.java
+++ demo/src/main/java/com/demo/domain/Posting.java
@@ -3,30 +3,25 @@ package com.demo.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.format.annotation.DateTimeFormat;
 
-import java.time.LocalDateTime;
-
-@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Setter @Getter
-public class Posting {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-    @CreatedDate
-    LocalDateTime createdAt;
-
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-    @LastModifiedDate
-    LocalDateTime modifedAt;
-
+public class Posting extends AuditingFields {
     private String title;
     private String content;
     private String author;
+
+    // 외부에서 사용 못하게 하려고함
+    protected Posting() {}
+    // 외부에서 사용불가
+    private Posting(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    // 이 메서드를 통해서만 엔티티의 인스턴스 만들기
+    public static Posting of(String title, String content, String author) {
+        return new Posting(title, content, author);
+    }
 }
