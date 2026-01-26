19bd21b47cc68a385095f425bf5fca4f312dd624
정성국 <ssoup0224@gmail.com>
2026-01-26 23:51:17 +0900

    edited user page controller

--- demo/src/main/java/com/demo/controller/page/UserPageController.java
+++ demo/src/main/java/com/demo/controller/page/UserPageController.java
@@ -1,18 +1,19 @@
 package com.demo.controller.page;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
 public class UserPageController {
-    @RequestMapping("/${page}")
+    @GetMapping("/${page}")
     public String page(@PathVariable String page) {
         return "user/" + page;
     }
 
-    @RequestMapping("/${page}/{id}")
+    @GetMapping("/${page}/{id}")
     public String page2(@PathVariable String page, @PathVariable Long id) {
         return "user/" + page;
     }
